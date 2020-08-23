6d43745e4250bbf3ea6c8e5199c1d594307ab830
Potato <68516497+potatonc@users.noreply.github.com>
2020-08-24 04:51:03 +0700

    Configuration profile

---
+++ menu/profile.sh
@@ -0,0 +1,27 @@
+# ~/.profile: executed by Bourne-compatible login shells.
+
+if [ "$BASH" ]; then
+  if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+  fi
+fi
+
+mesg n || true
+clear
+echo "   :::::::::   :::::::: ::::::::::: ::: ::::::::::: ::::::::" | lolcat
+echo "   :+:    :+: :+:    :+:    :+:   :+: :+:   :+:    :+:    :+:" | lolcat
+echo "   +:+    +:+ +:+    +:+    +:+  +:+   +:+  +:+    +:+    +:+" | lolcat
+echo "   +#++:++#+  +#+    +:+    +#+ +#++:++#++: +#+    +#+    +:+" | lolcat
+echo "   +#+        +#+    +#+    +#+ +#+     +#+ +#+    +#+    +#+" | lolcat
+echo "   #+#        #+#    #+#    #+# #+#     #+# #+#    #+#    #+#" | lolcat
+echo "   ###         ########     ### ###     ### ###     ########" | lolcat
+echo ""
+echo "============================================================"
+echo ""
+echo "                 ##########################" | lolcat
+echo "                 # Selamat Datang Kembali #" | lolcat
+echo "                 ##########      ##########" | lolcat
+echo "       # Ketik ☛ menu ☚ untuk menampilkan daftar menu #" | lolcat
+echo ""
+echo "                  === Credit by Potato ===" | lolcat
+echo ""

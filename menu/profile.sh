4ed176403bdd6d2737285ed06f6325310ca9c80b
Potato <68516497+potatonc@users.noreply.github.com>
2020-08-24 20:59:46 +0700

    Add files via upload

---
+++ menu/profile.sh
@@ -0,0 +1,34 @@
+# ~/.profile: executed by Bourne-compatible login shells.
+
+if [ "$BASH" ]; then
+  if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+  fi
+fi
+
+mesg n || true
+clear
+echo "   '########:::'#######::'########::::'###::::'########::'#######::" | lolcat
+echo "    ##.... ##:'##.... ##:... ##..::::'## ##:::... ##..::'##.... ##:" | lolcat
+echo "    ##:::: ##: ##:::: ##:::: ##:::::'##:. ##::::: ##:::: ##:::: ##:" | lolcat
+echo "    ########:: ##:::: ##:::: ##::::'##:::. ##:::: ##:::: ##:::: ##:" | lolcat
+echo "    ##.....::: ##:::: ##:::: ##:::: #########:::: ##:::: ##:::: ##:" | lolcat
+echo "    ##:::::::: ##:::: ##:::: ##:::: ##.... ##:::: ##:::: ##:::: ##:" | lolcat
+echo "    ##::::::::. #######::::: ##:::: ##:::: ##:::: ##::::. #######::" | lolcat
+echo "   ..::::::::::.......::::::..:::::..:::::..:::::..::::::.......:::" | lolcat
+echo "   ================================================================" | lolcat
+echo "       Selamat datang kembali $HOSTNAME" | lolcat
+echo ""
+echo "         ####################################################      " | lolcat
+echo "         #   Perintah :                                     #      " | lolcat
+echo "         #       menu   -   Untuk menampilkan daftar menu   #      " | lolcat
+echo "         #                                                  #      " | lolcat
+echo "         #   Info/Bug report :                              #      " | lolcat
+echo "         #       Telegram   -   aldi_nc                     #      " | lolcat
+echo "         #       Instagram  -   @aldi_nc                    #      " | lolcat
+echo "         #                                                  #      " | lolcat
+echo "         #                Credit by Potato©                 #      " | lolcat
+echo "         ####################################################      " | lolcat
+echo ""
+echo "   ================================================================" | lolcat
+echo ""

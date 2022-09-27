e5f60ac52586309d23e49561826cfae1aa94d720
Potato <68516497+potatonc@users.noreply.github.com>
2022-09-28 02:42:10 +0700

    Update profile.sh

--- menu/profile.sh
+++ menu/profile.sh
@@ -13,28 +13,4 @@ screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
 screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7400 --max-clients 500
 screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7500 --max-clients 500
 screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7600 --max-clients 500
-clear
-echo "   '########:::'#######::'########::::'###::::'########::'#######::" | lolcat
-echo "    ##.... ##:'##.... ##:... ##..::::'## ##:::... ##..::'##.... ##:" | lolcat
-echo "    ##:::: ##: ##:::: ##:::: ##:::::'##:. ##::::: ##:::: ##:::: ##:" | lolcat
-echo "    ########:: ##:::: ##:::: ##::::'##:::. ##:::: ##:::: ##:::: ##:" | lolcat
-echo "    ##.....::: ##:::: ##:::: ##:::: #########:::: ##:::: ##:::: ##:" | lolcat
-echo "    ##:::::::: ##:::: ##:::: ##:::: ##.... ##:::: ##:::: ##:::: ##:" | lolcat
-echo "    ##::::::::. #######::::: ##:::: ##:::: ##:::: ##::::. #######::" | lolcat
-echo "   ..::::::::::.......::::::..:::::..:::::..:::::..::::::.......:::" | lolcat
-echo "   ================================================================" | lolcat
-echo "       Selamat datang kembali $HOSTNAME" | lolcat
-echo ""
-echo "         ####################################################      " | lolcat
-echo "         #   Perintah :                                     #      " | lolcat
-echo "         #       menu   -   Untuk menampilkan daftar menu   #      " | lolcat
-echo "         #                                                  #      " | lolcat
-echo "         #   Info/Bug report :                              #      " | lolcat
-echo "         #       Telegram   -   aldi_nc                     #      " | lolcat
-echo "         #       Instagram  -   @aldi_nc                    #      " | lolcat
-echo "         #                                                  #      " | lolcat
-echo "         #                Credit by Potato©                 #      " | lolcat
-echo "         ####################################################      " | lolcat
-echo ""
-echo "   ================================================================" | lolcat
-echo ""
+p0t4t0

a27dc3bb6f24d7e5a63fe1cc960c83a4ee143b9e
Potato <68516497+potatonc@users.noreply.github.com>
2022-12-19 06:39:26 +0700

    Create profiless.sh

---
+++ menu/profiless.sh
@@ -0,0 +1,12 @@
+# ~/.profile: executed by Bourne-compatible login shells.
+
+if [ "$BASH" ]; then
+  if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+  fi
+fi
+
+mesg n || true
+clear
+neofetch
+$POTATO_UPDATE
